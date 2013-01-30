7a8e447c224c081ee5650195df150ea1f23f8e42
jacobhyphenated <jacobhyphenated@gmail.com>
2013-01-30 15:59:06 -0500

    Fix up some spring bean config and import issues.

--- src/test/java/com/hyphenated/card/dao/GameDaoTest.java
+++ src/test/java/com/hyphenated/card/dao/GameDaoTest.java
@@ -1,10 +1,11 @@
 package com.hyphenated.card.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
